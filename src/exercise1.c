ff478dc420d5ba0c7cdc48251c2c6a16ad52c7cd
Рябчун Егор Сергеевич ИП-512 <egorrybchun@gmail.com>
2025-10-10 16:11:21 +0700

    сделал функцию

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+float ceil_custom(float num) {
+    if (num == (int)num) {
+        return num;
+    }
+    if (num > 0) {
+        return (int)num + 1;
+    } else {
+        return (int)num;
+    }
+}
 
 int main(int argc, char *argv[]) {
 
     float array[10];
-    for(int i=0; i<10; i++){
-        array[i] = atof(argv[i+1]);
-    }
-    for(int i=0; i<10; i++){
-        printf("%f\n", array[i]);
+    for(int i = 0; i < 10; ++i) {
+        scanf("%f", &array[i]);
     }
     float result=0;
     for(int i=0; i<10; i++){
         result += array[i];
     }
     result = result/10;
-    result = (ceil(result * 100.0)) / 100.0;
+    result = (ceil_custom(result * 100.0)) / 100.0;
     printf("%.2f\n",result);
 
 }
