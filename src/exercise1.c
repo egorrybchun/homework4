d7fae40735a4d9430feaf2bed39699a099deab5c
Рябчун Егор Сергеевич ИП-512 <egorrybchun@gmail.com>
2025-10-14 09:26:30 +0700

    имправил файл makefile

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-float ceil_custom(float num) {
-    if (num == (int)num) {
-        return num;
-    }
-    if (num > 0) {
-        return (int)num + 1;
-    }
-}
 
-int main(int argc, char *argv[]) {
+int main() {
 
-    float array[10];
-    for(int i = 0; i < 10; ++i) {
-        scanf("%f", &array[i]);
-    }
-    float result=0;
-    for(int i=0; i<10; i++){
-        result += array[i];
-    }
-    result = result/10;
+ double array[10];
 
-    result = (ceil_custom(result * 100.0)) / 100.0;
+ for (int i = 0; i < 10; i++) {
+  scanf("%lf", &array[i]);
+ }
 
-    if((result > 442.67) && (result < 442.68)){
-        result = 442.66;
-    }
-    if((result < -442.66) && (result > -442.67)){
-        result = -442.66;
-    }
-    printf("%.2f\n",result);
+ double average = 0;
 
+ for (int i = 0; i < 10; i++) {
+  average += array[i];
+ }
+
+ average /= 10;
+
+ printf("%.3f", average);
 }
