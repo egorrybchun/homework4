267eed11ae27a83708d53ff29018f50980057826
Рябчун Егор Сергеевич ИП-512 <egorrybchun@gmail.com>
2025-10-21 19:08:39 +0700

    все исправил

--- src/array_function.c
+++ src/array_function.c
@@ -1,35 +1,18 @@
 #include "../include/array_function.h"
-#include<stdlib.h>
-int* join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+#include <stdlib.h>
 
-    int* dest = (int*)malloc((size_src1 + size_src2) * sizeof(int));
-
-
-    if (dest == NULL) {
-        return NULL;
-    }
-
-
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
     for (size_t i = 0; i < size_src1; i++) {
         dest[i] = src1[i];
     }
 
-
     for (size_t i = 0; i < size_src2; i++) {
         dest[size_src1 + i] = src2[i];
     }
-
-    return dest;
 }
 
 
-int* join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    int* dest = (int*)malloc((size_src1 + size_src2) * sizeof(int));
-
-    if (dest == NULL) {
-        return NULL;
-    }
-
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     size_t i1 = 0, i2 = 0, i3 = 0;
 
     while (i1 < size_src1 && i2 < size_src2) {
@@ -47,13 +30,11 @@ int* join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
     while (i2 < size_src2) {
         dest[i3++] = src2[i2++];
     }
-
-    return dest;
 }
 
 
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
-        if (size == 0) {
+    if (size == 0) {
         return;
     }
 
